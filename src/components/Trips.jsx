ca72ddd898aa4757f9061bc2e4243037a9f2861e
Zephaniah <zealorez@gmail.com>
2021-05-15 10:56:24 +0800

    retrieve routes of selected trips from db

--- src/components/Trips.jsx
+++ src/components/Trips.jsx
@@ -1,16 +1,31 @@
-import React, { useContext, useEffect } from "react";
-import { ClimbingContext, loadTrips } from "../store";
+import React, { useContext, useEffect, useState } from "react";
+import {
+  ClimbingContext,
+  loadTrips,
+  setSelectedTrip,
+  loadRoutes,
+} from "../store";
 
 const Trips = () => {
+  // const [selectedTripIndex, setSelectedTripIndex] = useState();
   const { store, dispatch } = useContext(ClimbingContext);
-  const { trips } = store;
+  const { trips, currentTripIndex} = store;
+
+  // display all trips upon loading of page
   useEffect(() => {
     loadTrips(dispatch);
   }, [dispatch]);
 
+
+  // display routes for selected trip
+  const displayRoutes = (tripIndex) => {
+    const tripId = trips[tripIndex].id
+    loadRoutes(dispatch, tripId);
+  };
+
   // check if trips has been been set in state
   if (trips.length > 0) {
-    const tripList = trips.map((trip) => {
+    const tripList = trips.map((trip, index) => {
       return (
         <div key={trip.id} className="card col-4" style={{ width: "18rem" }}>
           <img src="..." className="card-img-top" alt="..." />
@@ -20,9 +35,12 @@ const Trips = () => {
               Some quick example text to build on the card title and make up the
               bulk of the card's content.
             </p>
-            <a href="#" className="btn btn-primary">
+            <button
+              onClick={()=> displayRoutes(index)}
+              className="btn btn-primary"
+            >
               Go somewhere
-            </a>
+            </button>
           </div>
         </div>
       );
--- src/store.js
+++ src/store.js
@@ -2,12 +2,20 @@ import React, {useReducer} from 'react'
 import axios from 'axios'
 
 export const initialState = {
-  trips: []
+  trips: [],
+  routes: [],
+  currentTripIndex: null
 }
+
 export function climbingReducer(state, action){
   switch(action.type){
     case LOAD_TRIPS:
       return {...state, trips: action.payload}
+    case LOAD_ROUTES:
+      return {...state, routes: action.payload}
+    case SET_TRIP:
+      console.log(action.payload)
+      return {...state, currentTripIndex: action.payload}
     default:
       return state
   }
@@ -27,6 +35,8 @@ export function ClimbingProvider({children}) {
 
 // TYPES
 const LOAD_TRIPS = 'LOAD_TRIPS'
+const LOAD_ROUTES = 'LOAD_ROUTES'
+const SET_TRIP = 'SET_TRIP'
 
 
 // action functions
@@ -37,13 +47,36 @@ export function loadTripsAction(trips) {
   }
 }
 
+export function loadRoutesAction(routes) {
+  return {
+    type: LOAD_ROUTES,
+    payload:routes
+  }
+}
+
+export function setSelectedTrip(index) {
+  return {
+    type: SET_TRIP,
+    payload: index
+  }
+}
 
 
 const BACKEND_URL = 'http://localhost:3004'
 
 // axios requests
+
+// load all existing trips
 export function loadTrips(dispatch){
   axios.get(BACKEND_URL + '/trips').then((result)=> {
     dispatch(loadTripsAction(result.data.trips))
+  })
+}
+
+// load all routes for a chosen trip
+export function loadRoutes(dispatch, tripId) {
+  axios.get(BACKEND_URL + `/routes/${tripId}`).then((result)=> {
+    console.log(result.data.routes)
+    dispatch(loadRoutesAction(result.data.routes))
   })
 }
