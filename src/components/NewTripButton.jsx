56d5effb8b70a214f2a0f9112c7f4e6735444cc5
iannyip <ianyip@nullspace.com.sg>
2021-05-16 14:13:41 +0800

    added modal to NewTripButton component

--- src/components/NewTripButton.jsx
+++ src/components/NewTripButton.jsx
@@ -1,5 +1,35 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import { ClimbingContext } from "../store.js";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
 
 export default function NewTripButton() {
-  return <button className="btn btn-primary">Add New Trip</button>;
+  const { store, dispatch } = useContext(ClimbingContext);
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  const handleSubmit = () => {
+    console.log("Add submit code here");
+    setShow(false);
+  };
+
+  return (
+    <span>
+      <button className="btn btn-primary" onClick={handleShow}>
+        Add New Trip
+      </button>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+        <Modal.Footer>
+          <Button variant="primary" onClick={handleSubmit}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </span>
+  );
 }
