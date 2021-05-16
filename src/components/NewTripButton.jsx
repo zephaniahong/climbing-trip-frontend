321b21e46e09661882dc039ff5469b8c1bc4a16f
iannyip <ianyip@nullspace.com.sg>
2021-05-16 14:20:50 +0800

    Added input field change event handler

--- src/components/NewTripButton.jsx
+++ src/components/NewTripButton.jsx
@@ -8,11 +8,18 @@ export default function NewTripButton() {
   const [newtrip, setNewTrip] = useState("");
   const [show, setShow] = useState(false);
 
-  const handleClose = () => setShow(false);
+  const handleClose = () => {
+    setShow(false);
+    setNewTrip("");
+  };
   const handleShow = () => setShow(true);
   const handleSubmit = () => {
     console.log("Add submit code here");
     setShow(false);
+  };
+  const handleChange = (event) => {
+    console.log(newtrip);
+    setNewTrip(event.target.value);
   };
 
   return (
@@ -25,7 +32,13 @@ export default function NewTripButton() {
           <Modal.Title>Modal heading</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <input type="text" placeholder="New Trip Name" value={newtrip} />
+          <input
+            type="text"
+            placeholder="New Trip Name"
+            className="form-control"
+            value={newtrip}
+            onChange={handleChange}
+          />
         </Modal.Body>
         <Modal.Footer>
           <Button variant="primary" onClick={handleSubmit}>
