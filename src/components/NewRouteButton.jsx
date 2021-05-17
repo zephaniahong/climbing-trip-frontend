6f624152f6ee50614f933df650f2ff1f24648250
iannyip <ianyip@nullspace.com.sg>
2021-05-17 17:10:08 +0800

    added select dropdown

--- src/components/NewRouteButton.jsx
+++ src/components/NewRouteButton.jsx
@@ -4,9 +4,20 @@ import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
 
 export default function NewRouteButton() {
-  const { dispatch } = useContext(ClimbingContext);
+  const { state, dispatch } = useContext(ClimbingContext);
   const [newRoute, setNewRoute] = useState("");
+  const [newGrade, setNewGrade] = useState("");
   const [show, setShow] = useState(false);
+  const difficulties = [
+    1, 2, 3, 4, 5.0, 5.1, 5.2, 5.3, 5.4, 5.5, 5.6, 5.7, 5.8, 5.9, 5.1, 5.11,
+    5.12, 5.13, 5.14, 5.15,
+  ];
+
+  const difficultiesOptions = difficulties.map((grade) => {
+    return <option key={grade}>{grade}</option>;
+  });
+
+  const { routes } = state;
 
   const handleClose = () => {
     setShow(false);
@@ -28,7 +39,7 @@ export default function NewRouteButton() {
       </button>
       <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
-          <Modal.Title>Add a new trip!</Modal.Title>
+          <Modal.Title>Add a new route!</Modal.Title>
         </Modal.Header>
         <Modal.Body>
           <input
@@ -38,10 +49,18 @@ export default function NewRouteButton() {
             value={newRoute}
             onChange={handleInputChange}
           />
+          <select
+            class="custom-select"
+            onChange={(event) => {
+              setNewGrade(event.target.value);
+            }}
+          >
+            {difficultiesOptions}
+          </select>
         </Modal.Body>
         <Modal.Footer>
           <Button variant="primary" onClick={handleSubmit}>
-            Add trip!
+            Add route!
           </Button>
         </Modal.Footer>
       </Modal>
